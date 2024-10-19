ec4d4ee7b37b9a0a744d04dbc1abdbff22bc91d5
Aangelsepulveda <angelsepulvedasep@gmail.com>
2024-10-18 22:33:57 -0300

    Terminando crud de documentTypes

--- src/document-types/document-types.controller.ts
+++ src/document-types/document-types.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { DocumentTypesService } from './document-types.service';
-import { CreateDocumentTypeDto } from './dto/create-document-type.dto';
-import { UpdateDocumentTypeDto } from './dto/update-document-type.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common'
+import { DocumentTypesService } from './document-types.service'
+import { CreateDocumentTypeDto } from './dto/create-document-type.dto'
+import { UpdateDocumentTypeDto } from './dto/update-document-type.dto'
+import { DocumentType } from './entities/document-type.entity'
+import { ApiCreatedResponse } from '@nestjs/swagger'
 
 @Controller('document-types')
 export class DocumentTypesController {
@@ -9,26 +19,31 @@ export class DocumentTypesController {
 
   @Post()
   async create(@Body() createDocumentTypeDto: CreateDocumentTypeDto) {
-    return await this.documentTypesService.create(createDocumentTypeDto);
+    return await this.documentTypesService.create(createDocumentTypeDto)
   }
 
   @Get()
+  @ApiCreatedResponse({ type: DocumentType, isArray: true })
   async findAll() {
-    return await this.documentTypesService.findAll();
+    return this.documentTypesService.findAll()
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.documentTypesService.findOne(+id);
+  @ApiCreatedResponse({ type: DocumentType })
+  async findOne(@Param('id') id: string) {
+    return this.documentTypesService.findOne(id)
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDocumentTypeDto: UpdateDocumentTypeDto) {
-    return this.documentTypesService.update(+id, updateDocumentTypeDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateDocumentTypeDto: UpdateDocumentTypeDto,
+  ) {
+    return this.documentTypesService.update(id, updateDocumentTypeDto)
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.documentTypesService.remove(+id);
+  async remove(@Param('id') id: string) {
+    return this.documentTypesService.remove(id)
   }
 }
--- src/document-types/document-types.service.ts
+++ src/document-types/document-types.service.ts
@@ -1,29 +1,36 @@
 import { Injectable } from '@nestjs/common'
 import { CreateDocumentTypeDto } from './dto/create-document-type.dto'
 import { UpdateDocumentTypeDto } from './dto/update-document-type.dto'
-import { PrismaService } from 'src/prisma/prisma.service'
+import { PrismaService } from '../prisma/prisma.service'
 
 @Injectable()
 export class DocumentTypesService {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(createDocumentTypeDto: CreateDocumentTypeDto) {
-    return await this.prisma.documentType.create({ data: createDocumentTypeDto })
+    return this.prisma.documentType.create({ data: createDocumentTypeDto })
   }
 
   async findAll() {
-    return await this.prisma.documentType.findMany()
+    return this.prisma.documentType.findMany()
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} documentType`
+  async findOne(id: string) {
+    return this.prisma.documentType.findUnique({
+      where: {
+        id,
+      },
+    })
   }
 
-  update(id: number, updateDocumentTypeDto: UpdateDocumentTypeDto) {
-    return `This action updates a #${id} documentType`
+  async update(id: string, updateDocumentTypeDto: UpdateDocumentTypeDto) {
+    return this.prisma.documentType.update({
+      where: { id },
+      data: updateDocumentTypeDto,
+    })
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} documentType`
+  async remove(id: string) {
+    return this.prisma.documentType.delete({ where: { id } })
   }
 }
